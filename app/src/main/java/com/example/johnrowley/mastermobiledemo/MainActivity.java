0282525167dba26c6a02068e4ae0ad36cf99a83c
John Rowley <johnrowley@Johns-MacBook.local>
2018-04-22 10:58:30 +0100

    Added sample listView with sample listing initialised at start

--- app/src/main/java/com/example/johnrowley/mastermobiledemo/MainActivity.java
+++ app/src/main/java/com/example/johnrowley/mastermobiledemo/MainActivity.java
@@ -4,6 +4,8 @@ import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -15,6 +17,7 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,10 +26,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Setting this up as a test to confirm listings work
+        ListView myMovieListing = (ListView) findViewById(R.id.movieListing);
+        ArrayList listOfMovies = new ArrayList<String>();
+        listOfMovies.add("Spiderman");
+        listOfMovies.add("Superman");
+        listOfMovies.add("Batman");
+        listOfMovies.add("The Green Lantern");
+
+        Log.i("Array Listing Test ", listOfMovies.get(0).toString());
+
+
+        ArrayAdapter<String> arrayAdapterMovie = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, listOfMovies);
+
+        myMovieListing.setAdapter(arrayAdapterMovie);
+
         Log.i("Starting up ", "Application has started");
 
-        DownloadTask task = new DownloadTask();
-        task.execute("https://college-movies.herokuapp.com/");
+      //  DownloadTask task = new DownloadTask();
+     //   task.execute("https://college-movies.herokuapp.com/");
     }
 
 
@@ -80,38 +98,38 @@ public class MainActivity extends AppCompatActivity {
                 //Retrieve the movies - it is a list of objects, not a single object
                 JSONArray arr = new JSONArray(result);
 
-               //JSONObject jsonObject = new JSONObject(result);
+                //JSONObject jsonObject = new JSONObject(result);
 
-               // String weatherInfo = jsonObject.getString("title");
+                // String weatherInfo = jsonObject.getString("title");
 
-                Log.i("Movie Title", "testing movie");
+                Log.i("Movie Title", "Parsing Movie JSON Data");
 
-               // JSONArray arr = new JSONArray(weatherInfo);
+                // JSONArray arr = new JSONArray(weatherInfo);
 //arr.length()
 
                 //If we want to retreive all movies we would use: arr.length()
                 //But for demonstration purposes, only retrieving 10 items
-               for (int i = 0; i < 10; i++) {
+                for (int i = 0; i < 10; i++) {
 
                     JSONObject jsonPart = arr.getJSONObject(i);
 
-                   Log.i("Movie Ttle", jsonPart.getString("title"));
-                   Log.i("Movie Genre", jsonPart.getString("genre"));
+                    Log.i("Movie Ttle", jsonPart.getString("title"));
+                    Log.i("Movie Genre", jsonPart.getString("genre"));
 
-                   //Extract the running times - an object containing arrays
+                    //Extract the running times - an object containing arrays
 
-                   JSONObject runningTimes = jsonPart.getJSONObject("runningTimes");
+                    JSONObject runningTimes = jsonPart.getJSONObject("runningTimes");
 
-                   //Extract Monday
+                    //Extract Monday
 
-                   JSONArray mondayRunningTimes = runningTimes.getJSONArray("mon");
+                    JSONArray mondayRunningTimes = runningTimes.getJSONArray("mon");
 
-                   for(int j=0; j < mondayRunningTimes.length(); j++) {
+                    for (int j = 0; j < mondayRunningTimes.length(); j++) {
 
-                       //Json Array, extract the running time based on j index
-                       String runningTime = mondayRunningTimes.getString(j);
-                       Log.i("Monday Time",  runningTime);
-                   }
+                        //Json Array, extract the running time based on j index
+                        String runningTime = mondayRunningTimes.getString(j);
+                        Log.i("Monday Time", runningTime);
+                    }
 
 
                 }
@@ -121,7 +139,6 @@ public class MainActivity extends AppCompatActivity {
                 Log.i("MovieError", "retrieval error");
                 e.printStackTrace();
             }
-
 
 
         }
